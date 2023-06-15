9fa3b1fecb3b7dcc7a84320b62a97ac2033d2bfc
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-06-15 13:06:18 -0700

    Move rollArray to funtion

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,6 @@
-//Biz Logic for PlayerList
 function PlayerList() {
     this.players = {};
     this.currentId = 0;
-};
-
-PlayerList.prototype.startGame = function () {
-    this.players[0].currentTurn = true;
-    this.players[1].currentTurn = false;
 };
 
 PlayerList.prototype.assignId = function() {
@@ -26,11 +20,9 @@ PlayerList.prototype.findPlayer = function (id) {
     return false;
 };
 
-//Business logic for Player
-function Player(name){
-    this.name = name;
-    this.points = [];
-    this.currentTurn = true;
+PlayerList.prototype.startGame = function () {
+    this.players[0].currentTurn = true;
+    this.players[1].currentTurn = false;
 };
 
 PlayerList.prototype.holdRoll = function(){
@@ -43,13 +35,19 @@ PlayerList.prototype.holdRoll = function(){
     }
 };
 
+//Business logic for Player
+function Player(name){
+    this.name = name;
+    this.points = [];
+    // this.currentTurn = true;
+};
+
 Player.prototype.diceRoll = function (prevScore = 0){
     const numbers = [1,2,3,4,5,6];
     let number = numbers[Math.floor(Math.random() * numbers.length)];
     console.log(number);
     let talliedNumbers = this.points;
     talliedNumbers.push(number);
-    // this.points = talliedNumbers;
     console.log(talliedNumbers);
     let newTotal = 0;
     for (let i = 0; i < talliedNumbers.length; i++){
@@ -60,39 +58,40 @@ Player.prototype.diceRoll = function (prevScore = 0){
 
 
 
-// UI Logic
+//UI Logic
 function handleFormSubmission(event){
     event.preventDefault();
+    let player1Name = document.getElementById("player1").value;
+    console.log(player1Name);
+    let player2Name = document.getElementById("player2").value;
+    console.log(player2Name);
+    let player1 = new Player(player1Name);
+    console.log(player1)
+    let player2 = new Player(player2Name);
+    console.log(player2);
+    let welcomeOutput = document.getElementById("welcome-output"); 
+    welcomeOutput.append("Welcome " + player1Name + " and " + player2Name + " to the game of Pig Dice! The first player to reach 100 points wins! " + player1Name + ", roll the dice to start the game.")
 
-    let player1name = document.getElementById("player1").value;
-    let player2name = document.getElementById("player2").value;
-    let player1 = new Player(player1name);
-    let player2 = new Player(player2name);
-    // console.log(player1);
-    // console.log(player2);
-    let welcomeOutput = document.getElementById("welcome-output");
-    welcomeOutput.append("Welcome " + player1name + " and " + player2name + " to the game of Pig Dice! The first player to reach 100 points wins! Player 1 roll the dice to start the game!");
+    // let rollArray = document.getElementById("roll-array-output");
+    // rollArray.innerText = (player1.points);
 
-    let rollOutput = document.getElementById("roll-output");
-    rollOutput.append(player1.diceRoll.value);
+
+    function handleRollClick () {
+        let rollOutput = document.getElementById("roll-output");
+        rollOutput.innerText = (player1.diceRoll())
+        let rollArray = document.getElementById("roll-array-output");
+        rollArray.innerText = player1.points;
+    } 
+    document.querySelector("button#roll-button").addEventListener("click", handleRollClick);
 }
+
+function handleHoldClick () {
+    
+}
+
+
 
 window.addEventListener("load", function(){
     document.querySelector("form#name-form").addEventListener("submit", handleFormSubmission);
-
-    document.querySelector("button#roll-button").addEventListener("click", Player.diceRoll);
-
-    // document.getElementById("span#roll-result").innerText = newTotal;
+    // document.querySelector("form#name-form").addEventListener("submit", startGame);
 })
-
-
-// PL = new PlayerList();
-// c = new Player("C");
-// l = new Player("L");
-// PL.addPlayer(c);
-// PL.addPlayer(l);
-// c.diceRoll();
-// console.log(c.points);
-// c.diceRoll();
-// console.log(c.points);
-// console.log(PL.players);
