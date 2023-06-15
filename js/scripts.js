1f2cf98cc5614782d9b841d4cab61f5fd0b3bf1c
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-06-15 08:09:46 -0700

    Add roll function for player roll action

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,48 @@
-// Biz Logic
+//Biz Logic for PlayerList
 function PlayerList() {
     this.players = {};
     this.currentId = 0;
-}
+};
+
+PlayerList.prototype.assignId = function() {
+    this.currentId += 1;
+    return this.currentId;
+};
 
 PlayerList.prototype.addPlayer = function (player) {
     player.id = this.assignId();
     this.players[player.id] = player;
-}
-
-PlayerList.prototype.assignId = function () {
-    this.currentId += 1;
-    return this.currentId;
-}
+};
 
 PlayerList.prototype.findPlayer = function (id) {
     if (this.players[id] !== undefined) {
         return this.players[id];
     }
     return false;
-}
-
-function Player(name) {
-    this.name = name
-    this.points = 0
-    // this.currentId = 0
-}
-
-Player.prototype.addName = function (name) {
-    this.name = name
-}
-
-function DiceRoll() {
-    this.numbers = [1, 2, 3, 4, 5, 6];
 };
 
-DiceRoll.prototype.roll = function (prevScore = 0) {
-    let number = this.numbers[Math.floor(Math.random() * this.numbers.length)];
-    let newTotal = prevScore + number;
+//Business logic for Player
+function Player(name){
+    this.name = name;
+    this.points = [];
+};
+
+Player.prototype.diceRoll = function (prevScore = 0){
+    const numbers = [1,2,3,4,5,6];
+    let number = numbers[Math.floor(Math.random() * numbers.length)];
+    console.log(number);
+    let talliedNumbers = this.points;
+    talliedNumbers.push(number);
+    // this.points = talliedNumbers;
+    console.log(talliedNumbers);
+    let newTotal = 0;
+    for (let i = 0; i < talliedNumbers.length; i++){
+        newTotal += talliedNumbers[i];
+    }
     return newTotal;
 };
 
-function PointTally () {
-    this.points = 0;
-}
+// Biz Logic for PointTally
 
 PointTally.prototype.tallyPoints = function (rolls) {
     let diceRoll = new DiceRoll();
@@ -54,6 +53,20 @@ PointTally.prototype.tallyPoints = function (rolls) {
     return this.points;
 }
 
+
+// // UI Logic
+// function handleFormSubmission(event){
+// event.preventDefault();
+// }
+
+// window.addEventListener("load", function(){
+//     document.querySelector("form#name-form").addEventListener("submit", handleFormSubmission);
+//     document.querySelector("button#roll-button").addEventListener("click", diceRoll);
+//     // document.querySelector("span#roll-result").innerHTML = newTotal;
+// })
+
+
+
 const pointTally1 = new PointTally()
 const pointTally2 = new PointTally()
 
@@ -62,38 +75,3 @@ const result2 = pointTally2.tallyPoints();
 console.log(pointTally1);
 console.log(pointTally2);
 console.log(result1);
-
-
-// // UI Logic
-// function rollTheDice() {
-//     let newRoll = new DiceRoll();
-//     let result = newRoll.roll();
-//     document.getElementById("roll-result").append(result);
-// }
-
-// function handleFormSubmit(event) {
-//     event.preventDefault();
-// }
-
-// window.addEventListener("load", function () {
-//     document.querySelector("form#name-form").addEventListener("submit", handleFormSubmit);
-//     document.querySelector("button#roll-button").addEventListener("click", rollTheDice);
-// });
-
-
-
-// // console.log(result2);
-
-// // const diceRoll = new DiceRoll();
-// // const result = diceRoll.roll();
-// // console.log(result);
-
-
-// // let player1 = new Player("Casey");
-// // let player2 = new Player("Lindsay");
-// // let playerlist = new PlayerList();
-// // playerlist.addPlayer(player1);
-// // playerlist.addPlayer(player2);
-
-// // console.log(playerlist.players)
-
